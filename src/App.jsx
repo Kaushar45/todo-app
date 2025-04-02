f408394dc76b3b325dd2e1c4c8cd5b9a3487456f
Kaushar45 <45kaushar@gmail.com>
2025-04-02 20:44:10 +0530

    Initialize project setup with react + vite

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+function App() {
+ return (
+    <>
+  <section className='flex justify-center item-center mx-12 p-5 '>
+    <form className=''>
+      <input 
+      placeholder='Add Your Task' type='text'/>
+      <button>Add</button>
+    </form>
+  </section>
+    </>
+  )
+}
+
+export default App
